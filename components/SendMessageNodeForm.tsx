1b12c5d60e349d316da214da9d98043ce1a138cd
xellifi <xellifi.com@gmail.com>
2025-12-22 14:26:33 +0800

    Update Send DM Node

--- components/SendMessageNodeForm.tsx
+++ components/SendMessageNodeForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { MessageSquare } from 'lucide-react';
-import { supabase } from '../services/supabaseClient';
+import { supabase } from '../lib/supabase';
 
 interface Button {
     title: string;
